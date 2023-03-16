ad7028ef0f8c6dc334043c0f28f6d15f53abb5d7
NicolasFontana <nico.fontana12@gmail.com>
2023-03-16 20:54:08 -0300

    added click action

--- src/Components/Shared/Button/ButtonText/index.jsx
+++ src/Components/Shared/Button/ButtonText/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from '../buttons.module.css'
 
-function ButtonText({ type, label }) {
-  return <button type={type} className={styles.buttonText}>{label}</button>;
+function ButtonText({ clickAction, type, label }) {
+  return <button type={type} className={styles.buttonText} onClick={clickAction}>{label}</button>;
 }
 
 export default ButtonText;
