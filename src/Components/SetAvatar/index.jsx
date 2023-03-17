6721a26663e7168173c599caccd207a728caaa05
NicolasFontana <nico.fontana12@gmail.com>
2023-03-17 10:21:46 -0300

    setAvatar functionality

--- src/Components/SetAvatar/index.jsx
+++ src/Components/SetAvatar/index.jsx
@@ -2,34 +2,46 @@ import React, { useEffect, useState } from "react";
 import styles from "./index.module.css";
 import { Buffer } from "buffer";
 import Preloader from "Components/Shared/Preloader";
+import ButtonText from "Components/Shared/Button/ButtonText";
+import { ToastContainer, toast } from "react-toastify";
 
 function SetAvatar() {
   const [avatars, setAvatars] = useState([]);
+  const [selectedAvatar, setSelectedAvatar] = useState()
   const [isLoading, setIsLoading] = useState(true);
 
   const api = "https://api.multiavatar.com";
+  const setProfilePicture = () => {
+    console.log('asd')
+    console.log(selectedAvatar)
+    if(selectedAvatar === undefined) {
+      toast.error('Select an avatar please', {
+        position: "bottom-right",
+        theme: "dark"
+      })
+    }
+  }
   useEffect(() => {
     const data = [];
     const getAvatars = async () => {
       try {
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < 4; i++) {
           const response = await fetch(`${api}/${Math.round(Math.random() * 1000)}`);
           const image = await response.text();
           const buffer = Buffer(image);
           data.push(buffer.toString("base64"));
         }
         setAvatars(data);
-        // setIsLoading(false);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
     getAvatars();
   }, []);
+
   return isLoading ? (
-    <>
-      <Preloader>Loading avatars...</Preloader>
-    </>
+    <Preloader>Loading avatars...</Preloader>
   ) : (
     <div className={styles.container}>
       <div className={styles.titleContainer}>
@@ -38,13 +50,14 @@ function SetAvatar() {
       <div className={styles.avatars}>
         {avatars.map((avatar, index) => {
           return (
-            <div key={index} className={styles.avatar}>
-              <span>1</span>
+            <div key={index} className={`${styles.avatar} ${selectedAvatar === index ? styles.avatarSelected : ''}`} onClick={() => setSelectedAvatar(index)}>
               <img src={`data:image/svg+xml;base64,${avatar}`} alt="avatar" />
             </div>
           );
         })}
       </div>
+      <ButtonText type={'text'} label={'Select avatar'} clickAction={setProfilePicture} />
+      <ToastContainer />
     </div>
   );
 }
